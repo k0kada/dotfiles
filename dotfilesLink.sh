bf6b4854eb0e4d92c8ba5bb7ee0f2b3fce9be1c2
Koh Okada <koh.okada@gmail.com>
2016-09-20 23:52:29 +0900

    rewite dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+rm -rf ~/.vimrc
+rm -rf ~/.gitconfig
+rm -rf ~/.tmux.conf
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/ditfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
